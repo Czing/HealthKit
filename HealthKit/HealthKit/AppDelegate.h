225187456075a49fa561bb8fb89b2a1b46e6b301
chenyazhou <chenyazhou@dinfo.cn>
2016-10-27 13:54:35 +0800

    demo

---
+++ HealthKit/HealthKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HealthKit
+//
+//  Created by Cyzing on 16/9/18.
+//  Copyright © 2016年 Cyzing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HealthKit/HealthKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthKit
+//
+//  Created by Cyzing on 16/9/18.
+//  Copyright © 2016年 Cyzing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
